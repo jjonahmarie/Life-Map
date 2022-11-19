c6590045df98dc819cbe6f3742b2e8755a5eafe7
Jonah Evangelista <jonahevangelista@yahoo.com>
2022-11-18 23:06:48 -0700

    Initial Commit

---
+++ Life Map/Life_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Life_MapApp.swift
+//  Life Map
+//
+//  Created by Jonah Marie Evangelista on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Life_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
